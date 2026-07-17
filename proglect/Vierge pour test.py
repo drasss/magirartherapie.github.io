c56f6da3f178429541b48d63f3ca245456a396be
Aure <technodras@gmail.com>
2026-07-17 11:53:34 +0200

    start

--- proglect/Vierge pour test.py
+++
@@ -1,4 +0,0 @@
-import urllib.parse,urllib
-query = "Tokyo Ghoul Root A OST Disk1 #25 - Licht und Schatten"
-query=urllib.parse.quote(query)
-print(query)
--- proglect/recupmp3.bat
+++
@@ -1,4 +0,0 @@
-chcp 65001
-dir ..\mp3 /B>recup.txt
-chcp 1252
-exit
